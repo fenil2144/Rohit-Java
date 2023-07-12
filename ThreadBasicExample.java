4b7d0ebefb848123ee6be1b411cf345d45fda3d4
Fenil Shah <60958830+fenil2144@users.noreply.github.com>
2023-07-12 22:31:33 +0530

    Add files via upload

--- ThreadBasicExample.java
+++ ThreadBasicExample.java
@@ -7,7 +7,6 @@ class ThreadA extends Thread{
 				Thread.sleep(100);
 				System.out.println(Thread.currentThread()+" with i= "+i);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -15,17 +14,46 @@ class ThreadA extends Thread{
 	
 }
 
+class ThreadRunnable implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("ThreadRunnable "+Thread.currentThread());
+		for(int i=20;i>0;i--) {
+			try {
+				Thread.sleep(100);
+				System.out.println(Thread.currentThread()+" with i= "+i);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
+
 public class ThreadBasicExample {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.out.println("Main Method Thread "+Thread.currentThread());
 		
 		ThreadA threadA = new ThreadA();
 		ThreadA threadA2 = new ThreadA();
-		
+		threadA2.setPriority(9);
+
 		threadA.start();
 		threadA2.start();
+		
+		ThreadRunnable threadRunnable = new ThreadRunnable();
+		Thread threadUsingRunnable = new Thread(threadRunnable);
+		Thread threadUsingRunnable1 = new Thread(threadRunnable);
+		
+		threadUsingRunnable1.setDaemon(true);
+		threadA.join();
+		threadUsingRunnable.start();
+		
+
 	}
 
 }
