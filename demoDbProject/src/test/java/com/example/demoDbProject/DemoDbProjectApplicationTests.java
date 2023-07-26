a80ad4d6a8763af1b9a8018394dea4a7dfab5c9b
Fenil Shah <60958830+fenil2144@users.noreply.github.com>
2023-07-26 22:44:49 +0530

    Add files via upload

---
+++ demoDbProject/src/test/java/com/example/demoDbProject/DemoDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoDbProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
