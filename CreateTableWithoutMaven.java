bb652a8709642564bbae43c3347d5a00fb728c23
Fenil Shah <60958830+fenil2144@users.noreply.github.com>
2023-07-26 22:41:41 +0530

    Add files via upload

---
+++ CreateTableWithoutMaven.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTableWithoutMaven {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/rohitDb", "root", "root1234");
+		Statement statement = connection.createStatement();
+		
+		statement.execute("create table maven_dummy(id int,name varchar(30))");
+		
+	}
+
+}
